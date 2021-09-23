ddf4d6de90fa424758c1238482d95d7f2d38db33
NolanThrondset2022 <816059@mystma.org>
2021-09-23 14:18:53 -0500

    7

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 #WRITE YOUR CODE IN THIS FILE
-def topFiveMovies( a , b , c , d , e ):
-    return a + b + c + d + e
-topFiveMovies("pacific rim", "sound and fury", "once upon a time in hollywood", "kill bill", "princes's bride")
+def topFiveMovies():
+    return "pacific rim" + "sound and fury" + "once upon a time in hollywood" + "kill bill" + "princes's bride"
